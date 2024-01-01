05d1accfbe96e695cde04de6eb4e86aeaa6367d7
Thái Vĩnh Đạt <117071011+tvdat20004@users.noreply.github.com>
2024-01-01 23:55:38 +0700

    Update and rename solve_dlp.py to tool.py

--- polihg-hellman/tool.py
+++ polihg-hellman/tool.py
@@ -6,17 +6,15 @@ p = 255323935218170062404631265715751866767
 a = 165215949739654367733138784456328004818
 b = 67589529911274220250907684697333922377
 E = EllipticCurve(GF(p), [a,b])
+P = E(115093265778331772076313463823377371547, 204608502148379153747855373458343963619)
+Q = E(46932642630638585173854260473508367012, 118558123055213343390864876834385319021)
 
-P = E.gen(0)
-
-with open('cipher.txt', 'r') as f:
+with open('cipher.enc', 'r') as f:
     dataCipher = f.read()
     iv = bytes.fromhex(dataCipher[0:32])
     encrypted_flag = bytes.fromhex(dataCipher[32:])
 
-Q = E(41324044221887482254380150457319906053, 65969667437294267823394803695224442309)
-
-secret = P.discrete_log(Q)
+secret = discrete_log(Q, P, operation="+")
 assert P*secret == Q
 print(secret)
 
@@ -26,5 +24,5 @@ key = sha1.digest()[:16]
 cipher = AES.new(key, AES.MODE_CBC, iv) 
 recover = cipher.decrypt(encrypted_flag)
 
-with open('recoveredfile.pdf', 'wb') as file:
+with open('recovered.pdf', 'wb') as file:
     file.write(recover)
