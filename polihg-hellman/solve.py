b0d2523be33b42689855129fec050281b77b339f
Thái Vĩnh Đạt <117071011+tvdat20004@users.noreply.github.com>
2024-01-03 11:36:17 +0700

    Update solve.py

--- polihg-hellman/solve.py
+++ polihg-hellman/solve.py
@@ -3,20 +3,19 @@ from sage.all import *
 from Crypto.Cipher import AES
 import hashlib
 from tqdm import trange
-p = 255323935218170062404631265715751866767
-a = 165215949739654367733138784456328004818
-b = 67589529911274220250907684697333922377
+p = 115792089210356248762697446949407573530086143415290314195533631308867097853951
+a = 115792089210356248762697446949407573530086143415290314195533631308867097853948
+b = 1235004111951061474045987936620314048836031279781573542995567934901240450608
 
 E = EllipticCurve(GF(p), [a,b])
 
-P = E.gen(0)
-
-with open('cipher.txt', 'r') as f:
+P = E(58739589111611962715835544993606157139975695246024583862682820878769866632269,86857039837890738158800656283739100419083698574723918755107056633620633897772)
+Q = E(53389524449399713241908666754198583135391726383277973572010353430393882869587,96915749683251448875914358893119124077807308307116979366734609648027786948994)
+with open('cipher.enc', 'r') as f:
     dataCipher = f.read()
     iv = bytes.fromhex(dataCipher[0:32])
     encrypted_flag = bytes.fromhex(dataCipher[32:])
 
-Q = E(62346538674783463848877272148696432571, 250434738461211621900702825288932669267)
 def binary_search(array, value):
     n = len(array)
     left = 0
@@ -64,7 +63,7 @@ subgroup = []
 for prime, exponent in fac:
     P0 = (n // (prime ** exponent)) * P 
     Q0 = (n // (prime ** exponent)) * Q
-    x = BSGS_ECDLP(P0, Q0, E)
+    x = discrete_log(Q0, P0, operation='+')
     d.append(x)
     print(f"x = {x}  mod({prime**exponent})")
     subgroup.append(prime**exponent)
@@ -79,5 +78,5 @@ key = sha1.digest()[:16]
 cipher = AES.new(key, AES.MODE_CBC, iv) 
 recover = cipher.decrypt(encrypted_flag)
 
-with open('recoveredfile2.pdf', 'wb') as file:
+with open('recovered.pdf', 'wb') as file:
     file.write(recover)
